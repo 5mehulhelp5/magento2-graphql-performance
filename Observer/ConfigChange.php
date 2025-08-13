baed0d50f3ced4490bf6a085d918174271fcc242
Veysi YILDIZ <veysiyildiz@gmail.com>
2025-08-13 10:59:13 +0300

    fix: Update CacheWarmer method name from warm to warmupCache

--- Observer/ConfigChange.php
+++ Observer/ConfigChange.php
@@ -37,6 +37,6 @@ class ConfigChange implements ObserverInterface
         $this->cache->clean(['config_change']);
 
         // Re-warm cache if warming is enabled
-        $this->cacheWarmer->warm();
+        $this->cacheWarmer->warmupCache();
     }
 }
